49c3e86c06f68c8913b2636d73db2461629d476c
francesc.diaz <francesc.diaz@auleseebe.upc.edu>
2024-10-24 11:34:11 +0200

    pregunta 2

--- Pregunta2.R
+++ Pregunta2.R
@@ -1,1 +1,12 @@
+# valor esperat=1/lamda=100000
+t<-seq(0,100000,1)
+lambda <- 1/10000
+plot(t,pexp(t,lambda),type="s",col="red")
+1-pexp(13000,lambda)
+qexp(0.8,lambda)
 
+set.seed(85)
+simul<- rexp(100,lambda)
+mean(simul)
+median(simul)
+var(simul)
